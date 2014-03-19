41189895863ea07a4f2209a11b999c8377f42961
SankaGIT <nuwan.ras@gmail.com>
2014-03-19 15:28:37 +0530

    edit

--- module/Album/src/Album/Model/OrderTable.php
+++ module/Album/src/Album/Model/OrderTable.php
@@ -3,6 +3,11 @@ namespace Album\Model;
 
 use Zend\Db\TableGateway\TableGateway;
 use Zend\Validator\Db\RecordExists;
+use Zend\Db\TableGateway\AbstractTableGateway;
+use Zend\Db\Adapter\Adapter;
+use Zend\Db\ResultSet\ResultSet;
+use Zend\Db\Sql\Sql;
+use Zend\Db\Sql\Expression;
 class OrderTable
 {
     protected $tableGateway;
@@ -37,25 +42,42 @@ class OrderTable
         );
 
         $id = (int)$order->order_no;
-        echo $id;
-        $this->tableGateway->insert($data);
-        //check for primary key constraints here ********************************
-        /*if ($id == 0) {
+        //echo $id;
+
+        $sql = new Sql($this->tableGateway->adapter);
+        $select = $sql->select();
+        $select->from('orders');
+        $select->where(array('order_no' => $id));
+        $statement = $sql->prepareStatementForSqlObject($select);
+        $results = $statement->execute();
+        $resultSet = new ResultSet;
+        $resultSet->initialize($results);
+        $size = $results->count();
+        //$size2 = $resultSet->count();
+        //$state = false;
+
+        /*foreach($resultSet as $row){
+            $state = true;
+            error_log ( $row->brn . PHP_EOL );
+        }*/
+
+        error_log("brn test "."results->count=".$size);
+        //error_log("brn test "."resultSet->count=".$size2);
+        //error_log("brn test "."state".$state);
+        error_log("MyDebug"." Success");
+
+
+        if ($size == 0) {
             $this->tableGateway->insert($data);
+            //error_log("brn test ". "Add Business");
         } else {
-            if ($this->getBusiness($id)) {
-                $this->tableGateway->update($data, array('brn' => $id));
+            if ($this->getOrder($id)) {
+                $this->tableGateway->update($data, array('order_no' => $id));
+                //error_log("brn test ". "Edit Business");
             } else {
                 throw new \Exception('Form id does not exist');
             }
         }
-        /*
-        if ($id == 0) {
-            $this->tableGateway->insert($data);
-        }
-        else{
-            $this->tableGateway->update($data, array('brn' => $id));
-        }*/
 
     }
 
