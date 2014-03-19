c02c7862b412ac3465f1e03a977d138081dc48af
SankaGIT <nuwan.ras@gmail.com>
2014-03-19 15:09:31 +0530

    edit business

--- module/Album/src/Album/Model/BusinessTable.php
+++ module/Album/src/Album/Model/BusinessTable.php
@@ -3,6 +3,12 @@ namespace Album\Model;
 
 use Zend\Db\TableGateway\TableGateway;
 use Zend\Validator\Db\RecordExists;
+use Zend\Db\TableGateway\AbstractTableGateway;
+use Zend\Db\Adapter\Adapter;
+use Zend\Db\ResultSet\ResultSet;
+use Zend\Db\Sql\Sql;
+use Zend\Db\Sql\Expression;
+
 class BusinessTable
 {
     protected $tableGateway;
@@ -37,25 +43,43 @@ class BusinessTable
         );
 
         $id = (int)$business->brn;
-        echo $id;
-        //check for primary key constraints here ********************************
-        /*if ($id == 0) {
+
+        error_log("MyDebug ".$id);
+
+        $sql = new Sql($this->tableGateway->adapter);
+        $select = $sql->select();
+        $select->from('businesses');
+        $select->where(array('brn' => $id));
+        $statement = $sql->prepareStatementForSqlObject($select);
+        $results = $statement->execute();
+        $resultSet = new ResultSet;
+        $resultSet->initialize($results);
+        $size = $results->count();
+        //$size2 = $resultSet->count();
+        //$state = false;
+
+        /*foreach($resultSet as $row){
+            $state = true;
+            error_log ( $row->brn . PHP_EOL );
+        }*/
+
+        error_log("brn test "."results->count=".$size);
+        //error_log("brn test "."resultSet->count=".$size2);
+        //error_log("brn test "."state".$state);
+        error_log("MyDebug"." Success");
+
+
+        if ($size == 0) {
             $this->tableGateway->insert($data);
+            error_log("brn test ". "Add Business");
         } else {
             if ($this->getBusiness($id)) {
                 $this->tableGateway->update($data, array('brn' => $id));
+                error_log("brn test ". "Edit Business");
             } else {
                 throw new \Exception('Form id does not exist');
             }
         }
-        /*
-        if ($id == 0) {
-            $this->tableGateway->insert($data);
-        }
-        else{
-            $this->tableGateway->update($data, array('brn' => $id));
-        }*/
-
     }
 
     public function deleteBusiness($id)
