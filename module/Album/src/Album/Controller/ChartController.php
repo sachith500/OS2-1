382c3e0ecb9d540368ff309b735d0c51c523b0eb
vipula-11 <vipula.11@cse.mrt.ac.lk>
2014-03-22 01:45:14 +0530

    2 chart linked to db

--- module/Album/src/Album/Controller/ChartController.php
+++ module/Album/src/Album/Controller/ChartController.php
@@ -8,6 +8,7 @@
 
 namespace Album\Controller;
 
+use Zend\Db\ResultSet\ResultSet;
 use Zend\Mvc\Controller\AbstractActionController;
 
 class ChartController extends AbstractActionController
@@ -20,11 +21,27 @@ class ChartController extends AbstractActionController
 
     public function orderDistAction()
     {
+
+        $sm = $this->getServiceLocator();
+        $testCon = new TestController();
+        $query = 'select * from large_orders;';
+        $resultSet = $testCon->runSql($query, $sm);
+        $nlo= $resultSet->count();
+
+        $query = 'select * from mail_orders;';
+        $resultSet = $testCon->runSql($query, $sm);
+        $nmo= $resultSet->count();
+
+        $query = 'select * from vip_orders;';
+        $resultSet = $testCon->runSql($query, $sm);
+        $nvo= $resultSet->count();
         $type = "PieChart";
+
+
         $data = array(
-            array('type' => 'Mail Order', 'amount' => 2),
-            array('type' => 'Large Order', 'amount' => 1),
-            array('type' => 'Retail Order', 'amount' => 4)
+            array('type' => 'Mail Order', 'amount' => $nmo),
+            array('type' => 'Large Order', 'amount' => $nlo),
+            array('type' => 'VIP Order', 'amount' => $nvo)
         );
 
         return array(
@@ -35,11 +52,25 @@ class ChartController extends AbstractActionController
 
     public function customerDistAction()
     {
+        $sm = $this->getServiceLocator();
+        $testCon = new TestController();
+        $query = 'select * from l_o_customers;';
+        $resultSet = $testCon->runSql($query, $sm);
+        $nlo= $resultSet->count();
+
+        $query = 'select * from mo_customers;';
+        $resultSet = $testCon->runSql($query, $sm);
+        $nmo= $resultSet->count();
+
+        $query = 'select * from vip_customers;';
+        $resultSet = $testCon->runSql($query, $sm);
+        $nvo= $resultSet->count();
         $type = "PieChart";
+
         $data = array(
-            array('type' => 'Mail Order Customers', 'amount' => 2),
-            array('type' => 'Large Order Customers', 'amount' => 1),
-            array('type' => 'VIP Customers', 'amount' => 4)
+            array('type' => 'Mail Order Customers', 'amount' => $nmo),
+            array('type' => 'Large Order Customers', 'amount' => $nlo),
+            array('type' => 'VIP Customers', 'amount' => $nvo)
         );
 
         return array(
@@ -62,8 +93,8 @@ class ChartController extends AbstractActionController
         );
 
         return array(
-            'type'=>$type,
-            'data'=>$data,
+            'type' => $type,
+            'data' => $data,
         );
     }
 
