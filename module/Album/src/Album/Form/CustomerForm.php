4192350cb24e95044d5efe18203e4e3b6baf5ab5
Sachith Hasaranga Seneviratne <sachith500@gmail.com>
2014-03-23 12:28:25 +0530

    Added several fieds to customer form to get large order details

--- module/Album/src/Album/Form/CustomerForm.php
+++ module/Album/src/Album/Form/CustomerForm.php
@@ -23,6 +23,9 @@ class CustomerForm extends Form
         $this->add(array(
             'type' => 'Zend\Form\Element\Select',
             'name' => 'customer_type',
+            'attributes' => array(
+                'id' => 'customer_type',
+            ),
             'options' => array(
             'label' => 'Customer Type',
             'value_options' => array(
@@ -99,6 +102,59 @@ class CustomerForm extends Form
                 'type'  => 'submit',
                 'value' => 'Go',
                 'id' => 'submitbutton',
+            ),
+        ));
+
+
+        //Large order fields
+        $this->add(array(
+            'name' => 'id',
+            'attributes' => array(
+                'type'  => 'text',
+                'class' => 'largeorder'
+            ),
+            'options' => array(
+                'label' => 'ID',
+            ),
+        ));
+        $this->add(array(
+            'name' => 'credit_Limit',
+            'attributes' => array(
+                'type'  => 'text',
+                'class' => 'largeorder'
+            ),
+            'options' => array(
+                'label' => 'Credit Limit',
+            ),
+        ));
+        $this->add(array(
+            'name' => 'credit_Balance',
+            'attributes' => array(
+                'type'  => 'text',
+                'class' => 'largeorder'
+            ),
+            'options' => array(
+                'label' => 'Credit Balance',
+            ),
+        ));
+        $this->add(array(
+            'name' => 'brn',
+            'attributes' => array(
+                'type'  => 'text',
+                'class' => 'largeorder'
+            ),
+            'options' => array(
+                'label' => 'BusinessRegistration Number',
+            ),
+        ));
+        $this->add(array(
+            'name' => 'emp_id',
+            'attributes' => array(
+                'type'  => 'text',
+                'class' => 'largeorder'
+            ),
+            'options' => array(
+                'label' => 'Employee ID',
             ),
         ));
     }
