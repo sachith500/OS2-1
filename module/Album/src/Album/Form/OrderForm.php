ee0986fdea57b1a32c26fdfeb65a7010c44267b2
Sachith Hasaranga Seneviratne <sachith500@gmail.com>
2014-03-24 11:53:58 +0530

    Added dropdown to order table

--- module/Album/src/Album/Form/OrderForm.php
+++ module/Album/src/Album/Form/OrderForm.php
@@ -19,6 +19,22 @@ class OrderForm extends Form
                 'label' => 'Order Number',
             ),
         ));
+        //Dropdown menu to select the order type
+        $this->add(array(
+            'type' => 'Zend\Form\Element\Select',
+            'name' => 'order_type',
+            'attributes' => array(
+                'id' => 'order_type',
+            ),
+            'options' => array(
+                'label' => 'Order Type',
+                'value_options' => array(
+                    '0' => 'Large Order',
+                    '1' => 'Mail Order',
+                    '2' => 'VIP',
+                ),
+            )
+        ));
         $this->add(array(
             'name' => 'date',
             'attributes' => array(
